42eeb57a1e3fbe83826714c19dc8adf6c56a1ed1
newbots <hanikarim808@gmail.com>
2016-03-15 12:35:57 +0330

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -244,7 +244,7 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {164985762},--Sudo users
+	    sudo_users = {178990421},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
